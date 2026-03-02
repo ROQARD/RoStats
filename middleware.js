28dff958a7f821a6ff60efe4d156fbd9bac894ec
ROQARD <rooqard@gmail.com>
2026-03-02 15:46:19 +0000

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -71,7 +71,7 @@ const html = `<!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="google-adsense-account" content="ca-pub-1937157010205619">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1937157010205619" crossorigin="anonymous"></script>
     <title>RoStats</title>
     <script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
     <style>
